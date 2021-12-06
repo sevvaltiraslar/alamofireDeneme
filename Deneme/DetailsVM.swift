57fb531382c85803894f0cdf352431ade773f441
sevvaltiraslar <sevvaltiraslar@gmail.com>
2021-12-06 20:02:52 +0300

    denemee

---
+++ Deneme/DetailsVM.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsVM.swift
+//  Deneme
+//
+//  Created by SEVVAL on 6.12.2021.
+//
+
+import Foundation
+
+
+class DetailsVM {
+    var secilenData: TekliData!
+}
---
+++ Deneme/MainVM.swift
@@ -0,0 +1,14 @@
+//
+//  MainVM.swift
+//  Deneme
+//
+//  Created by SEVVAL on 6.12.2021.
+//
+
+import Foundation
+
+class MainVM {
+    var icerikler: [TekliData] = []
+    var baslik = [String]()
+    var yorum = [String]()
+}
